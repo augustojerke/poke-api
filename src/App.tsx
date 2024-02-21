79478a81c0939492f7e8e264f76ac044899a9012
Augusto <augustowieles@gmail.com>
2024-02-21 12:56:23 -0300

    input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Input } from "./components/ui/input"
 
 function App() {
   return (
-    <h1 className="">Teste</h1>
+    <main className="bg-slate-950">
+      <div className="flex justify-start align-items p-10">
+        <Input className="w-80 text-white bg-slate-900 border-slate-800" placeholder="Nome do Pokemon"/>
+      </div>
+    </main>
   ) 
 }
 
