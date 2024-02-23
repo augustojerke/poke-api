ba18437d3d9d773924674d3884198e3132efc2ed
Augusto <augustowieles@gmail.com>
2024-02-23 00:45:37 -0300

    layout e listagem ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,44 @@ import { Button } from "./components/ui/button"
 import { Input } from "./components/ui/input"
 import { Search } from "lucide-react"
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 interface Pokemons {
   name: string,
   mainImage: string
+  id: number,
 }
 
 function App() {
+
+  useEffect(() => {
+    getPokemons();
+  }, [])
 
   const [pokemons, setPokemons] = useState<Pokemons[]>([]);
 
   async function getPokemons() {
     try {
-      const pokemonsList = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=1400&offset=0");
+      const pokemonsList = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=1350&offset=0");
       const pokemonsResults = pokemonsList.data.results;
-
-      for(let i = 0; i < pokemonsList.data.count; i++){
+  
+      for (let i = 0; i < pokemonsList.data.count; i++) {
         const pokemonAtributtes = await axios.get(pokemonsResults[i].url);
-
+  
         const newPokemon: Pokemons = {
           name: pokemonsResults[i].name,
-          mainImage: pokemonAtributtes.data.sprites.front_default
-        }
+          mainImage: pokemonAtributtes.data.sprites.front_default,
+          id: pokemonAtributtes.data.id
+        };  
 
-        setPokemons((pokes) => [...pokes, newPokemon])
+        setPokemons((pokes) => [...pokes, newPokemon]);
       }
-
-      setPokemons(pokemonsResults);
 
     } catch (error) {
       console.error("Erro ao obter a lista de Pokémons:", error);
     }
   }
-
+  
   return (
     <main className="bg-slate-950">
       <div className="flex justify-start items-center p-10 space-x-4">
@@ -45,13 +49,13 @@ function App() {
           Buscar Pokémon
         </Button>        
       </div>
-      <div className="grid justify-center items-center grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 px-5 ml-4">
+      <div className="grid justify-center items-center grid-cols-2 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5 px-5 ml-4">
           {pokemons?.map((poke, i) => (
-            <div key={i} className="bg-slate-300 w-56 h-40 mx-2 my-2 border-4 border-slate-400 fl ex-col justify-center items-center
-                                      text-center hover:border-green-700">
-              <img className="mx-auto my-auto" src={poke.mainImage} alt={"Imagem " + poke.name} loading="lazy"/>
-              <h3 className="text-xl"><b>{poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}</b></h3>
-            </div>
+              <div key={i} className="bg-slate-800 w-56 h-40 mx-2 my-2 border-4 border-slate-400 fl ex-col justify-center items-center
+                                        text-center hover:border-green-700 rounded transition-transform transform-gpu hover:scale-105">
+                <img className="mx-auto my-auto" src={poke.mainImage} alt={"Imagem " + poke.name} loading="lazy"/>
+                <h3 className="text-xl text-white"><b>{poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}</b></h3>
+              </div>
           ))}
       </div>
     </main>
