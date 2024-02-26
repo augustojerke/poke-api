7dc8f15bd35a89bf6b1315a64bc3ca7bfed3edda
augustojerke <augustowieles@gmail.com>
2024-02-26 19:55:56 -0300

    ajuste para mobile

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ function App() {
           Buscar Pokémon
         </Button>        
       </div>
-      <div className="grid justify-center items-center grid-cols-2 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5 px-5 ml-4">
+      <div className="grid justify-center items-center grid-cols-1 sm:grid-cols-1 md:grid-cols-3 xl:grid-cols-5 px-20 xl:px-5 ml-4">
         <Dialog open={modal}>
           {pokemons?.map((poke, i) => (
             <DialogTrigger>
